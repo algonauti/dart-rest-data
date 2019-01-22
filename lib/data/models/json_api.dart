a4a89bb19ae17fa77fb268f556b9cf2f8a312aca
Davide Papagni <davide@algonauti.com>
2019-01-22 22:54:36 +0100

    method to retrieve a relationship's id

--- lib/data/models/json_api.dart
+++ lib/data/models/json_api.dart
@@ -9,5 +9,14 @@ class JsonApiModel implements Model {
   Map<String, dynamic> get attributes => jsonApiDocument.attributes;
   Map<String, dynamic> get relationships => jsonApiDocument.relationships;
 
+  @override
   String get id => attributes['id'];
+
+  String idFor(String relationshipName) {
+    if (relationships.containsKey(relationshipName)) {
+      Map<String, dynamic> data = relationships[relationshipName]['data'];
+      return data['id'];
+    } else
+      return null;
+  }
 }
