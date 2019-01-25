18c29e5885cb8a5690dd93b47963ebeefcea7153
Davide Papagni <davide@algonauti.com>
2019-01-26 00:14:50 +0100

    throw custom http exceptions based on status code

--- lib/data/adapters/mixins/http.dart
+++ lib/data/adapters/mixins/http.dart
@@ -7,13 +7,24 @@ mixin Http {
   var headers = Map<String, String>();
 
   String checkAndDecode(http.Response response) {
+    String method = response.request.method;
+    String url = response.request.url.toString();
+    int code = response.statusCode;
+    String body = response.body;
     if (response.statusCode == 200) {
       return utf8.decode(response.bodyBytes);
+    } else if (response.statusCode == 204) {
+      return null;
+    } else if (response.statusCode == 400) {
+      throw BadRequestException(method, url);
+    } else if (response.statusCode == 403) {
+      throw ForbiddenException(method, url);
+    } else if (response.statusCode == 404) {
+      throw NotFoundException(method, url);
+    } else if (response.statusCode == 422) {
+      throw UnprocessableException(method, url, body);
     } else {
-      String method = response.request.method;
-      String url = response.request.url.toString();
-      throw Exception(
-          "[$method $url failed] status=${response.statusCode}, body=${response.body}");
+      throw HttpException(method, url, statusCode: code, responseBody: body);
     }
   }
 
@@ -36,4 +47,49 @@ mixin Http {
     var url = Uri.https(hostname, path);
     return await http.patch(url, headers: headers, body: body);
   }
+}
+
+class HttpException implements Exception {
+  String method;
+  String url;
+  String responseBody;
+  int statusCode;
+
+  HttpException(this.method, this.url, {this.statusCode, this.responseBody});
+
+  @override
+  String toString() {
+    var msg = "$method $url returned $statusCode";
+    if (responseBody != null) msg += " with body: $responseBody";
+    return msg;
+  }
+}
+
+class BadRequestException extends HttpException {
+  BadRequestException(String method, String url) : super(method, url);
+
+  @override
+  String toString() => "Bad request: $method $url";
+}
+
+class ForbiddenException extends HttpException {
+  ForbiddenException(String method, String url) : super(method, url);
+
+  @override
+  String toString() => "Forbidden request: $method $url";
+}
+
+class NotFoundException extends HttpException {
+  NotFoundException(String method, String url) : super(method, url);
+
+  @override
+  String toString() => "Resource not found: $method $url";
+}
+
+class UnprocessableException extends HttpException {
+  UnprocessableException(String method, String url, String responseBody)
+      : super(method, url, responseBody: responseBody);
+
+  @override
+  String toString() => "Unprocessable request: $method $url";
 }
