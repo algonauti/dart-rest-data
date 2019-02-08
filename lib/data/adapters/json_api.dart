804e33dff2fa16c9f04dbb4c6ca15d37697a4d43
Davide Papagni <davide@algonauti.com>
2019-02-08 17:27:19 +0100

    JsonApiAdapter: extend find method with caching (+)
    - return requested document from cache if present
    - after fetching, store document in cache
    - do not lookup cache when forceReload is true

--- lib/data/adapters/json_api.dart
+++ lib/data/adapters/json_api.dart
@@ -14,7 +14,23 @@ class JsonApiAdapter extends Adapter with Http {
   }
 
   @override
-  Future<JsonApiDocument> find(String endpoint, String id) async {
+  Future<JsonApiDocument> find(String endpoint, String id,
+      {bool forceReload = false}) async {
+    if (forceReload == true) return _fetchAndCache(endpoint, id);
+    JsonApiDocument cached = peek(endpoint, id);
+    if (cached != null)
+      return cached;
+    else
+      return _fetchAndCache(endpoint, id);
+  }
+
+  Future<JsonApiDocument> _fetchAndCache(String endpoint, String id) async {
+    JsonApiDocument fetched = await _fetch(endpoint, id);
+    cache(endpoint, fetched);
+    return fetched;
+  }
+
+  Future<JsonApiDocument> _fetch(String endpoint, String id) async {
     final response = await httpGet("$apiPath/$endpoint/$id");
     String payload = checkAndDecode(response);
     return serializer.deserialize(payload);
