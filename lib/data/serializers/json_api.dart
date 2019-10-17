53e7086e2b8345c30c1e968d03789a36195360d1
Davide Papagni <davide@algonauti.com>
2019-10-17 23:55:06 +0200

    JsonApiDocument: let idFor() and typeFor() return null when no data (+)
    - DRY-up with dataForHasOne() which returns an empty map when null

--- lib/data/serializers/json_api.dart
+++ lib/data/serializers/json_api.dart
@@ -77,23 +77,24 @@ class JsonApiDocument {
 
   bool get isNew => id == null;
 
+  Map<String, dynamic> dataForHasOne(String relationshipName) =>
+      relationships.containsKey(relationshipName)
+          ? (relationships[relationshipName]['data'] ?? Map<String, dynamic>())
+          : Map<String, dynamic>();
+
   String idFor(String relationshipName) =>
-      (relationships.containsKey(relationshipName))
-          ? relationships[relationshipName]['data']['id']
-          : null;
+      dataForHasOne(relationshipName)['id'];
 
   String typeFor(String relationshipName) =>
-      (relationships.containsKey(relationshipName))
-          ? relationships[relationshipName]['data']['type']
-          : null;
+      dataForHasOne(relationshipName)['type'];
 
-  Iterable<String> idsFor(String relationshipName) {
-    if (relationships.containsKey(relationshipName)) {
-      Iterable<dynamic> data = relationships[relationshipName]['data'];
-      return data.map((record) => record['id']);
-    } else
-      return List<String>();
-  }
+  Iterable<dynamic> dataForHasMany(String relationshipName) =>
+      relationships[relationshipName]['data'] ?? List();
+
+  Iterable<String> idsFor(String relationshipName) =>
+      relationships.containsKey(relationshipName)
+          ? dataForHasMany(relationshipName).map((record) => record['id'])
+          : List<String>();
 
   Iterable<JsonApiDocument> includedDocs(String type, [Iterable<String> ids]) {
     ids ??= idsFor(type);
