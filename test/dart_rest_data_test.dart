1969ab1cbd31ae7afb8e2dfc0f092147da76d2c6
Davide Papagni <davide@algonauti.com>
2019-11-25 18:08:24 +0100

    add very basic tests

--- test/dart_rest_data_test.dart
+++ test/dart_rest_data_test.dart
@@ -1,12 +1,34 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:rest_data/rest_data.dart';
+import 'package:test/test.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  var createJsonApiDocument =
+      () => JsonApiDocument('unique_ID_1', 'model_type_1', {
+            'attribute_one': 'value_one',
+            'attribute_two': 'value_two',
+          }, {
+            'has_one_relationship': {
+              'data': {'id': 'unique_ID_2', 'type': 'model_type_2'}
+            },
+            'has_many_relationhip': {
+              'data': [
+                {'id': 'unique_ID_3', 'type': 'job_assignments'},
+                {'id': 'unique_ID_4', 'type': 'job_assignments'},
+              ]
+            },
+          });
+
+  test('create a new JsonApiDocument', () {
+    expect(createJsonApiDocument(), TypeMatcher<JsonApiDocument>());
+  });
+
+  test('create a new JsonApiModel', () {
+    Model model = JsonApiModel(createJsonApiDocument());
+    expect(model, TypeMatcher<JsonApiModel>());
+  });
+
+  test('create a new JsonApiAdapter', () {
+    Adapter adapter = JsonApiAdapter('host.example.com', '/path/to/rest/api');
+    expect(adapter, TypeMatcher<JsonApiAdapter>());
   });
 }
